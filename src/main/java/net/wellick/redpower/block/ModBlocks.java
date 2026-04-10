55ecf4e7478788df95811f29eec1c87c88759995
wellick <aurel0000@proton.me>
2026-04-10 19:27:35 +0200

    update

--- src/main/java/net/wellick/redpower/block/ModBlocks.java
+++ src/main/java/net/wellick/redpower/block/ModBlocks.java
@@ -35,6 +35,13 @@ public class ModBlocks {
 
             ));
 
+    public static final DeferredBlock<Block> BISMUTH_DEEPSLATE_ORE = registerBlock("bismuth_deepslate_ore",
+            () -> new DropExperienceBlock(UniformInt.of(2, 4), BlockBehaviour.Properties.of()
+                    .strength(3f)
+                    .requiresCorrectToolForDrops().sound(SoundType.STONE)
+
+            ));
+
     private static <T extends Block> DeferredBlock<T> registerBlock(String name, Supplier<T> block){
         DeferredBlock<T> toReturn = BLOCKS.register(name, block);
         registerBlockItem(name, toReturn);
